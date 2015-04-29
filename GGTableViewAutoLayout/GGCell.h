55106718d493c80ddb3de58f47b0825fee9ec025
zero <osmk@qq.com>
2015-04-29 21:14:34 +0800

    build

---
+++ GGTableViewAutoLayout/GGCell.h
@@ -0,0 +1,13 @@
+//
+//  GGCell.h
+//  GGTableViewAutoLayout
+//
+//  Created by __无邪_ on 15/4/28.
+//  Copyright (c) 2015年 __无邪_. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface GGCell : UITableViewCell
+
+@end
